6fc7fe1188a23d553ca695e04fceaf3e527478b2
whytdan <aktan.r.a@gmail.com>
2021-07-19 21:16:14 +0600

    remove unused code

--- api/categories.js
+++ api/categories.js
@@ -1,16 +1,5 @@
 import axios from 'axios';
 
-export const getAllCategorySlugs = async () => {
-  const { data } = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}/categories`
-  );
-  return data.map((category) => ({
-    params: {
-      slug: category.slug,
-    },
-  }));
-};
-
 export const getCategoryTitle = async (slug) => {
   const { data } = await axios.get(
     `${process.env.NEXT_PUBLIC_API_URL}/categories?slug=${slug}`
